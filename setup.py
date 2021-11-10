051e4146965030623a416c0161fbd515c3f70bdd
softyoungha <softyoungha@github.com>
2021-11-10 09:55:44 +0900

    setup.cfg 설정 완료

--- setup.py
+++ setup.py
@@ -1,35 +1,12 @@
-#-*- coding:utf-8 -*-
-from setuptools import setup, find_packages
+# -*- coding:utf-8 -*-
+from setuptools import setup
 from pip._internal.req import parse_requirements
 
 install_requires = [
-      str(r.requirement)
-      for r in parse_requirements('requirements.txt', session='hack')
-]
+                       str(r.requirement)
+                       for r in parse_requirements('requirements.txt', session='hack')
+                   ] + [
+                       'pywin32 >= 1.0 ; platform_system=="Windows"',
+                   ]
 
-setup(name='pyefriend',
-      version='1.0.1',
-      author='Youngha Park',
-      author_email='proyoungha@naver.com',
-      install_requires=install_requires,
-      url='https://github.com/softyoungha',
-      license='MIT',
-      description="Python API link to 'efriend expert'",
-      long_description=open('README.md', encoding='utf-8').read(),
-      long_description_content_type='text/markdown',
-      keywords=['pyefriend', 'rebalancing', 'efriend-expert'],
-      py_modules=['pyefriend', 'rebalancing'],
-      python_requires='>=3',
-      packages=find_packages(include=['pyefriend',
-                                      'rebalancing',
-                                      'rebalancing.*']),
-      package_data={
-            'rebalancing': [
-                  'config.template.yml',
-                  'data/*.csv',
-            ]},
-      entry_points={
-            'console_scripts': [
-                  'rebalancing=rebalancing.__main__:main',
-            ],
-      })
+setup(install_requires=install_requires)
