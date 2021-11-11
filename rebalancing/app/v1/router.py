1d0710950ce31b60cf5b6ee2f4165537d23f5829
softyoungha <softyoungha@github.com>
2021-11-11 19:53:33 +0900

    tags 순서 수정

--- rebalancing/app/v1/router.py
+++ rebalancing/app/v1/router.py
@@ -6,9 +6,6 @@ from .report.router import r as report_router
 
 r = APIRouter(prefix='/v1')
 
-r.include_router(report_router)
-r.include_router(database_router)
-r.include_router(setting_router)
-
-# set tags(ordering을 위해서)
-r.tags = ['v1']
+r.include_router(setting_router, tags=['v1'])
+r.include_router(database_router, tags=['v1'])
+r.include_router(report_router, tags=['v1'])
