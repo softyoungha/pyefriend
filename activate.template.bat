2f5ddef7ceabfdd89e956820779c89f902918539
softyoungha <softyoungha@github.com>
2021-11-12 09:28:30 +0900

    bat 파일 수정

--- activate.template.bat
+++ activate.template.bat
@@ -3,5 +3,7 @@
 set REBAL_HOME=.
 set REBAL_PASSWORD=password
 
+:: [주의] 관리자모드로 실행한 cmd.exe에서 venv를 활성화시킨 후 사용하세요
+:: `python -m venv venv` 커맨드 실행시 다음의 경로에 위치합니다.
 :: Activate Python
 .\venv\Scripts\activate
--- run_api.bat
+++ run_api.bat
@@ -1,7 +1,5 @@
 @echo off
 
-:: Activate Python
-activate.bat
-
+:: [주의] 관리자모드로 실행한 cmd.exe에서 venv를 활성화시킨 후 사용하세요
 :: Run Fastapi
 uvicorn rebalancing.api:app --host 0.0.0.0 --port 8000 --reload
